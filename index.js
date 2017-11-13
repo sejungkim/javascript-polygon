df49a52e5afa743d5d9c68c8b4d56ae3a4eeb2f8
hannarita <harugio@naver.com>
2017-11-13 20:58:31 +0900

    다각형의 넓이를 계산하는 함수 완료함

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var circleSize = 10*10*Math.PI;
-console.log("원의 넓이: " + circleSize);
+function circleSize(rad) {
+    var size = rad*rad*Math.PI;
+    console.log("원의 넓이: "+size);
+}
 
-var rectSize = 10*15/2;
-console.log("사각형의 넓이: " + rectSize);
+function rectSize(width,height) {
+    var size = width*height/2;
+    console.log("사각형의 넓이: "+size);
+}
 
-var trapeSize = (10+5)*12/2;
-console.log("사다리꼴의 넓이 " + trapeSize);
+function trapeSize(upper,bottom,height) {
+    if(arguments.length<=2){
+        console.log('3개의 인자가 필요합니다');
+    }
+    var size = upper*bottom*height/2;
+    console.log("사다리꼴의 넓이: "+size);
+}
+
+circleSize(10);
+rectSize(10,15);
+trapeSize(10,5,12);
+
