1aded7fb365fcc1e251537d2615b95535c667d4e
hannarita <harugio@naver.com>
2017-11-14 14:30:20 +0900

    다각형 넓이 구하기 함수의 인수 조건을 변경함

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 function circleSize(rad) {
-    var size = rad*rad*Math.PI;
-    console.log("원의 넓이: "+size);
+    if(typeof rad === 'string'){
+        console.log('숫자형타입만 계산이 가능합니다');
+    } else if(arguments.length===0){
+        console.log('최소 한가지 값이 필요합니다')
+    } else if(rad <= 0){
+        console.log('반지름은 0보다 커야합니다');
+    }
+    else {
+        var size = rad * rad * Math.PI;
+        console.log("원의 넓이: " + size);
+    }
 }
 
 function rectSize(width,height) {
-    var size = width*height/2;
-    console.log("사각형의 넓이: "+size);
+    if(typeof width=== 'string'||typeof height === 'string'){
+        console.log('숫자형타입만 계산이 가능합니다');
+    } else if(arguments.length===0){
+        console.log('최소 한가지 값이 필요합니다')
+    } else if(width<= 0 || height<= 0){
+        console.log('길이값과 높이값은 0보다 커야합니다');
+    }
+    else {
+        var size = width * height / 2;
+        console.log("사각형의 넓이: " + size);
+    }
 }
 
 function trapeSize(upper,bottom,height) {
     if(arguments.length<=2){
         console.log('3개의 인자가 필요합니다');
     }
+
     var size = upper*bottom*height/2;
     console.log("사다리꼴의 넓이: "+size);
 }
 
-circleSize(10);
-rectSize(10,15);
+circleSize();
+rectSize(-1,15);
 trapeSize(10,5,12);
-
