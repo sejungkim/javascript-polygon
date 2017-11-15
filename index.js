a65d294dc62e4a735bfc0902d1e31293968ca728
hannarita <harugio@naver.com>
2017-11-15 10:37:39 +0900

    modify: declaration var size in function calculateCircleSize

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-function circleSize(rad) {
-    if(typeof rad === 'string'){
-        console.log('숫자형타입만 계산이 가능합니다');
-    } else if(arguments.length===0){
-        console.log('최소 한가지 값이 필요합니다')
-    } else if(rad <= 0){
-        console.log('반지름은 0보다 커야합니다');
-    }
-    else {
-        var size = rad * rad * Math.PI;
+function calculateCircleSize(radius1,radius2) {
+    var size = 0;
+    if(arguments.length === 1){
+        size = radius1 * radius1 * Math.PI;
         console.log("원의 넓이: " + size);
+    }
+    if(arguments.length === 2){
+        for(var i = radius1; i <= radius2; i++){
+            console.log('반지름이 '+i+'인 원의 넓이는 :'+(i*i*Math.PI));
+        }
     }
 }
 
-function rectSize(width,height) {
-    if(typeof width=== 'string'||typeof height === 'string'){
+function calculateRectSize(width,height) {
+    if(typeof width === 'string'||typeof height === 'string'){
         console.log('숫자형타입만 계산이 가능합니다');
     } else if(arguments.length===0){
         console.log('최소 한가지 값이 필요합니다')
@@ -26,7 +25,7 @@ function rectSize(width,height) {
     }
 }
 
-function trapeSize(upper,bottom,height) {
+function calculateTrapeSize(upper,bottom,height) {
     if(arguments.length<=2){
         console.log('3개의 인자가 필요합니다');
     }
@@ -35,6 +34,17 @@ function trapeSize(upper,bottom,height) {
     console.log("사다리꼴의 넓이: "+size);
 }
 
-circleSize();
-rectSize(-1,15);
-trapeSize(10,5,12);
+
+
+function getArea(shape, para1, para2, para3) {
+    switch(shape){
+        case 'circle': calculateCircleSize(para1, para2);
+            break;
+        case 'rect': calculateRectSize(para1, para2);
+            break;
+        case 'trape': calculateTrapeSize(para1, para2, para3);
+            break;
+    }
+}
+
+getArea('circle',1,10);
