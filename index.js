2dbbab3887d842958974a4d39f43abdd627617b6
sejungkim <smilesejung@gmail.com>
2018-03-12 17:38:54 +0900

    Fix function checkInputValue
    - check invalid input value (string type, blank, negative number and number of input)
    - add try-catch statement in calculate function

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-function checkNumberOfInput(validNum, inputVal) {
-    return (validNum === inputVal.length) ?  1 : 0;
+function checkInputValue(validNum, inputVal) {
+    let inputValArray = Array.from(inputVal);
+    if(!inputValArray.every(elem => toString.call(elem) === "[object Number]")) {
+        throw "숫자형 타입만 계산이 가능합니다.";
+    }
+    if(inputValArray.length === 0) {
+        throw "최소 한가지 값이 필요합니다.";
+    }
+    if(!inputValArray.every(elem => elem > 0)) {
+        throw "입력값은 0보다 커야 합니다.";
+    }
+    if(validNum !== inputValArray.length) {
+        throw `${validNum}개의 인자가 필요합니다.`;
+    }
+    return true;
 }
 function calculateCircle(radius) {
     let result;
-    if(!checkNumberOfInput(1, arguments)) {
-        result = "1개의 인자가 필요합니다.";
+    try {
+        checkInputValue(1, arguments);
     }
-    else {
-        result = radius * radius * Math.PI;
+    catch(e) {
+        console.log(e);
+        return false;
     }
-    return console.log(result);
+    result = radius * radius * Math.PI;
+    console.log(result);
+    return true;
 }
 function calculateSquare(width, height) {
     let result;
-    if(!checkNumberOfInput(2, arguments)) {
-        result = "2개의 인자가 필요합니다.";
+    try {
+        checkInputValue(2, arguments);
     }
-    else {
-        result = width * height;
+    catch(e) {
+        console.log(e);
+        return false;
     }
-    return console.log(result);
+    result = width * height;
+    console.log(result);
+    return true;
 }
 function calculateTrapezoid(upper, lower, height) {
     let result;
-    if(!checkNumberOfInput(3, arguments)) {
-        result = "3개의 인자가 필요합니다.";
+    try {
+        checkInputValue(3, arguments);
     }
-    else {
-        result = (upper + lower) * height;
+    catch(e) {
+        console.log(e);
+        return false;
     }
-    return console.log(result);
+    result = (upper + lower) * height / 2;
+    console.log(result);
+    return true;
 }
 
 calculateCircle(10);
