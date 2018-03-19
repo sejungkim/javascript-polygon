550b36477d8b0774cb571d928760c9749eac5514
sejungkim <smilesejung@gmail.com>
2018-03-19 17:31:36 +0900

    Fix some as better expression
    - Use typeof for check about value of undefined in numberOfInput function
    - Fix order of parameters for readability in 'checkInputValue' function
    - Use exclamation mark to control v2 in 'circle' part of getArea function

--- index.js
+++ index.js
@@ -8,17 +8,18 @@ function checkEvery(targetArr, funcName) {
 }
 
 function numberOfInput(targetArr) {
-  return (targetArr.filter(elem => elem !== undefined)).length;
+  return (targetArr.filter(elem => typeof elem !== 'undefined')).length;
 }
 
-function checkInputValue(validNum, inputValArray) {
-  if (!numberOfInput(inputValArray)) {
+function checkInputValue(inputValArray, validNum) {
+  let numberOfValidInput = numberOfInput(inputValArray);
+  if (!numberOfValidInput) {
     throw "최소 한가지 값이 필요합니다.";
   }
-  if (validNum !== numberOfInput(inputValArray)) {
+  if (numberOfValidInput !== validNum) {
     throw `${validNum}개의 인자가 필요합니다.`;
   }
-  if (!checkEvery(inputValArray, elem => typeof elem === "number")) {
+  if (!checkEvery(inputValArray, elem => typeof elem === 'number')) {
     throw "숫자형 타입만 계산이 가능합니다.";
   }
   if (!checkEvery(inputValArray, elem => elem > 0)) {
@@ -27,19 +28,19 @@ function checkInputValue(validNum, inputValArray) {
 }
 
 function calculateCircle(radius) {
-  checkInputValue(1, [radius]);
+  checkInputValue([radius], 1);
   let result = radius * radius * Math.PI;
   console.log(result);
 }
 
 function calculateSquare(width, height) {
-  checkInputValue(2, [width, height]);
+  checkInputValue([width, height], 2);
   let result = width * height;
   console.log(result);
 }
 
 function calculateTrapezoid(upper, lower, height) {
-  checkInputValue(3, [upper, lower, height]);
+  checkInputValue([upper, lower, height], 3);
   let result = (upper + lower) * height / 2;
   console.log(result);
 }
@@ -47,9 +48,7 @@ function calculateTrapezoid(upper, lower, height) {
 function getArea(shape, v1, v2, v3) {
   switch (shape) {
     case 'circle':
-      if (v2 === undefined) {
-        calculateCircle(v1);
-      }
+      if (!v2) calculateCircle(v1);
       else {
         for (let i = 0; i < v2; i++) {
           calculateCircle(v1 + i);
